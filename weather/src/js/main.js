0c96d13064c494bbcdc63a270dfb5dd77eb9a795
xandeer <appdwj@icloud.com>
2017-01-20 23:44:13 +0800

    feat(location): add a button to toggle Celsius and Fahrenhei

--- weather/src/js/main.js
+++ weather/src/js/main.js
@@ -14,19 +14,46 @@ window.onload = function() {
     }
   }
 
+  function c2f(c) {
+    return Math.round(c * 1.8 + 32);
+  }
+
+  function f2c(f) {
+    return Math.round((f - 32) / 1.8);
+  }
+
   function render(data) {
     var channel = data.query.results.channel;
     var location = channel.description;
-    var condition = channel.item.condition.temp + ' °' + channel.units.temperature;
     var text = channel.item.condition.text;
     var imgUrl = channel.item.description.match(/http.*[.]gif/)[0];
+    var temp = channel.item.condition.temp;
+    if (channel.units.temperature == 'F') {
+      temp = f2c(channel.item.condition.temp);
+    }
 
     var html = '<div class="weather-location">' + location + '</div>' +
-      '<div class="weather-condition">' + condition + '</div>' +
+      '<div class="weather-condition"><span id="weather-temp">' + temp +
+      '</span> °<a href="javascript: void(0);" id="weather-toggle" class="weather-celsius weather-unit">C</a></div>' +
       '<div class="weather-img"><img src="' + imgUrl + '"></div>' +
       '<div class="weather-text">' + text + '</div>';
 
     weatherContent.innerHTML = html;
+
+    var weatherToggle = document.getElementById('weather-toggle');
+    weatherToggle.addEventListener('click', function(e) {
+      var weatherTemp = document.getElementById('weather-temp');
+      if (this.classList.contains('weather-celsius')) {
+
+        weatherTemp.innerHTML = c2f(weatherTemp.innerHTML);
+        this.classList.remove('weather-celsius');
+        this.innerHTML = 'F';
+      } else {
+        weatherTemp.innerHTML = f2c(weatherTemp.innerHTML);
+        this.classList.add('weather-celsius');
+        this.innerHTML = 'C';
+      }
+    });
   }
 
   function getDate(lat, long) {
