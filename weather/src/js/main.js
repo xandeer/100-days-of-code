9653090f0ec0144f4746cee97bd42bbdaa3c36ae
xandeer <appdwj@icloud.com>
2017-01-20 22:58:36 +0800

    feat(location): finish weather

--- weather/src/js/main.js
+++ weather/src/js/main.js
@@ -1,126 +1,68 @@
-// const data = {
-//   "query": {
-//     "count": 1,
-//     "created": "2014-05-03T03:57:53Z",
-//     "lang": "en-US",
-//     "results": {
-//       "channel": {
-//         "title": "Yahoo! Weather - Tebrau, MY",
-//         "link": "http://us.rd.yahoo.com/dailynews/rss/weather/Tebrau__MY/*http://weather.yahoo.com/forecast/MYXX0004_c.html",
-//         "description": "Yahoo! Weather for Tebrau, MY",
-//         "language": "en-us",
-//         "lastBuildDate": "Sat, 03 May 2014 11:00 am MYT",
-//         "ttl": "60",
-//         "location": {
-//           "city": "Tebrau",
-//           "country": "Malaysia",
-//           "region": ""
-//         },
-//         "units": {
-//           "distance": "km",
-//           "pressure": "mb",
-//           "speed": "km/h",
-//           "temperature": "C"
-//         },
-//         "wind": {
-//           "chill": "32",
-//           "direction": "170",
-//           "speed": "4.83"
-//         },
-//         "atmosphere": {
-//           "humidity": "66",
-//           "pressure": "982.05",
-//           "rising": "0",
-//           "visibility": "9.99"
-//         },
-//         "astronomy": {
-//           "sunrise": "6:57 am",
-//           "sunset": "7:06 pm"
-//         },
-//         "image": {
-//           "title": "Yahoo! Weather",
-//           "width": "142",
-//           "height": "18",
-//           "link": "http://weather.yahoo.com",
-//           "url": "http://l.yimg.com/a/i/brand/purplelogo//uh/us/news-wea.gif"
-//         },
-//         "item": {
-//           "title": "Conditions for Tebrau, MY at 11:00 am MYT",
-//           "lat": "1.58",
-//           "long": "103.74",
-//           "link": "http://us.rd.yahoo.com/dailynews/rss/weather/Tebrau__MY/*http://weather.yahoo.com/forecast/MYXX0004_c.html",
-//           "pubDate": "Sat, 03 May 2014 11:00 am MYT",
-//           "condition": {
-//             "code": "28",
-//             "date": "Sat, 03 May 2014 11:00 am MYT",
-//             "temp": "32",
-//             "text": "Mostly Cloudy"
-//           },
-//           "description": "\n<img src=\"http://l.yimg.com/a/i/us/we/52/28.gif\"/><br />\n<b>Current Conditions:</b><br />\nMostly Cloudy, 32 C<BR />\n<BR /><b>Forecast:</b><BR />\nSat - Scattered Thunderstorms. High: 32 Low: 26<br />\nSun - Thunderstorms. High: 33 Low: 27<br />\nMon - Scattered Thunderstorms. High: 32 Low: 26<br />\nTue - Thunderstorms. High: 32 Low: 26<br />\nWed - Scattered Thunderstorms. High: 32 Low: 27<br />\n<br />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Tebrau__MY/*http://weather.yahoo.com/forecast/MYXX0004_c.html\">Full Forecast at Yahoo! Weather</a><BR/><BR/>\n(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)<br/>\n",
-//           "forecast": [{
-//             "code": "38",
-//             "date": "3 May 2014",
-//             "day": "Sat",
-//             "high": "32",
-//             "low": "26",
-//             "text": "Scattered Thunderstorms"
-//           }, {
-//             "code": "4",
-//             "date": "4 May 2014",
-//             "day": "Sun",
-//             "high": "33",
-//             "low": "27",
-//             "text": "Thunderstorms"
-//           }, {
-//             "code": "38",
-//             "date": "5 May 2014",
-//             "day": "Mon",
-//             "high": "32",
-//             "low": "26",
-//             "text": "Scattered Thunderstorms"
-//           }, {
-//             "code": "4",
-//             "date": "6 May 2014",
-//             "day": "Tue",
-//             "high": "32",
-//             "low": "26",
-//             "text": "Thunderstorms"
-//           }, {
-//             "code": "38",
-//             "date": "7 May 2014",
-//             "day": "Wed",
-//             "high": "32",
-//             "low": "27",
-//             "text": "Scattered Thunderstorms"
-//           }],
-//           "guid": {
-//             "isPermaLink": "false",
-//             "content": "MYXX0004_2014_05_07_7_00_MYT"
-//           }
-//         }
-//       }
-//     }
-//   }
-// };
+require('../sass/style.scss');
 
-// window.onload = {
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(successFunction,
-    errorFunction);
-} else {
-  alert(
-    'It seems like Geolocation, which is required for this page, is not enabled in your browser.'
-  );
-}
+window.onload = function() {
+  var weatherContent = document.getElementById('weather-content');
 
-function successFunction(position) {
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-  alert('Your latitude is :' + lat + ' and longitude is ' + long);
-}
+  function getPosition(success, error) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success,
+        error);
+    } else {
+      alert(
+        'It seems like Geolocation, which is required for this page, is not enabled in your browser.'
+      );
+    }
+  }
 
-function errorFunction(position) {
-  alert('Error!');
-}
+  function render(data) {
+    var channel = data.query.results.channel;
+    var location = channel.description;
+    var condition = channel.item.condition.temp + ' °' + channel.units.temperature;
+    var text = channel.item.condition.text;
+    var imgUrl = channel.item.description.match(/http.*[.]gif/)[0];
 
-// };
+    var html = '<div class="weather-location">' + location + '</div>' +
+      '<div class="weather-condition">' + condition + '</div>' +
+      '<div class="weather-img"><img src="' + imgUrl + '"></div>' +
+      '<div class="weather-text">' + text + '</div>';
+
+    weatherContent.innerHTML = html;
+  }
+
+  function getDate(lat, long) {
+    var request = new XMLHttpRequest();
+    request.open('GET',
+      'https://simple-weather.p.mashape.com/weatherdata?lat=' + lat +
+      '&lng=' + long, true);
+    request.setRequestHeader('X-Mashape-Key',
+      'kZv5pogHZimshNygATAaaKsJ3Ykdp1hFMNyjsnJOqnoEsDXA5F');
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        render(data);
+      } else {
+        // We reached our target server, but it returned an error
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+
+    request.send();
+  }
+
+  function successFunction(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    getDate(lat, long);
+  }
+
+  function errorFunction(position) {
+    alert('Error!');
+  }
+
+  getPosition(successFunction, errorFunction);
+};
