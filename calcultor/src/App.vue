617d5425ecd1a3e726a3ce7d698052de68a0cf85
xandeer <appdwj@icloud.com>
2017-02-05 20:21:43 +0800

    feat(location): finish the style

--- calcultor/src/App.vue
+++ calcultor/src/App.vue
@@ -1,26 +1,47 @@
-<template>
-  <div id="app">
-    <!-- <hello></hello> -->
-  </div>
+<template lang='pug'>
+  #app
+    .container
+      header Simple Calculator
+      calculator.calc
+      footer
+        | Coded by
+        a(href='http://xandeer.me')  Kevin
 </template>
 
 <script>
-// import Hello from './components/Hello';
+import calculator from './components/calculator/calculator';
 
 export default {
   name: 'app',
   components: {
-    // Hello,
+    calculator,
   },
 };
 </script>
 
 <style>
-#app {
+.container {
+  height: 5rem;
+  margin-top: 0.5rem;
+}
+.calc {
   width: 3rem;
-  height: 4rem;
-  margin: 1rem auto;
+  height: 4.04rem;
+  margin: 0.2rem auto;
   border: 2px solid RGBA(142, 142, 142, 1.00);
   border-radius: 5px;
+}
+
+header {
+  text-align: center;
+  height: 0.5rem;
+  line-height: 0.5rem;
+  font-weight: bold;
+  font-size: 0.34rem;
+  font-style: italic;
+}
+
+footer {
+  text-align: center;
 }
 </style>
---
+++ calcultor/src/components/calculator/calculator.vue
@@ -0,0 +1,43 @@
+<template lang='pug'>
+#calc
+  .display-area
+    .answer 0
+    .expression 0
+  table.disable-select
+    tbody
+      tr
+        td.gray AC
+        td.gray CE
+        td.gray %
+        td.operator ÷
+      tr
+        td 7
+        td 8
+        td 9
+        td.operator ×
+      tr
+        td 4
+        td 5
+        td 6
+        td.operator -
+      tr
+        td 1
+        td 2
+        td 3
+        td.operator +
+      tr
+        td(colspan='2') 0
+        td .
+        td.operator =
+</template>
+
+<script>
+
+export default {
+  name: 'calculator',
+};
+</script>
+
+<style lang='scss' rel='stylesheet/scss'>
+  @import 'calculator.scss';
+</style>
