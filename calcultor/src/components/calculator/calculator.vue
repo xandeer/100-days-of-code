02a21a3c4d003d1fa17de583e0561e801285b92f
xandeer <appdwj@icloud.com>
2017-02-05 21:33:13 +0800

    feat(location): finish clean functions

--- calcultor/src/components/calculator/calculator.vue
+++ calcultor/src/components/calculator/calculator.vue
@@ -1,40 +1,99 @@
 <template lang='pug'>
 #calc
   .display-area
-    .answer 0
-    .expression 0
+    .current {{ current }}
+    .expression {{ expr }}
   table.disable-select
     tbody
       tr
-        td.gray AC
-        td.gray CE
-        td.gray %
-        td.operator ÷
+        td.danger(@click='allClear') AC
+        td.danger(@click='clearEntry') CE
+        td.operator(@click='compute("%")') %
+        td.operator(@click='compute("/")') ÷
       tr
-        td 7
-        td 8
-        td 9
-        td.operator ×
+        td(@click='compute(7)') 7
+        td(@click='compute(8)') 8
+        td(@click='compute(9)') 9
+        td.operator(@click='compute("*")') ×
       tr
-        td 4
-        td 5
-        td 6
-        td.operator -
+        td(@click='compute(4)') 4
+        td(@click='compute(5)') 5
+        td(@click='compute(6)') 6
+        td.operator(@click='compute("-")') -
       tr
-        td 1
-        td 2
-        td 3
-        td.operator +
+        td(@click='compute(1)') 1
+        td(@click='compute(2)') 2
+        td(@click='compute(3)') 3
+        td.operator(@click='compute("+")') +
       tr
-        td(colspan='2') 0
-        td .
-        td.operator =
+        td(colspan='2' @click='compute(0)') 0
+        td(@click='compute(".")') .
+        td.operator(@click='compute("=")') =
 </template>
 
 <script>
 
+function isOperator(n) {
+  let ret = false;
+  switch (n) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+    case '=':
+      ret = true;
+      break;
+    default:
+  }
+
+  return ret;
+}
+
+function isDot(n) {
+  return n === '.';
+}
+
 export default {
   name: 'calculator',
+  data() {
+    return {
+      current: 0,
+      expr: 0,
+      answer: 0,
+      last: 0,
+    };
+  },
+  methods: {
+    allClear() {
+      this.current = 0;
+      this.expr = 0;
+      this.answer = 0;
+      this.last = 0;
+      this.num = 0;
+    },
+    clearEntry() {
+      this.current = 0;
+    },
+    compute(n) {
+      if ((isOperator(this.last) && isOperator(n)) ||
+        (isDot(this.last) && isDot(n))) {
+        return;
+      }
+
+      if (isOperator(n)) {
+        this.num = this.current;
+        this.current = n;
+      } else {
+        this.current = n;
+      }
+
+      this.expr += n;
+
+
+      this.last = n;
+    },
+  },
 };
 </script>
 
