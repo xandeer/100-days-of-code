b6452bb035e5245bdc41d9721a1deb575c1b6486
xandeer <appdwj@icloud.com>
2017-02-08 09:40:15 +0800

    feat(location): init

---
+++ tic-tac-toe/src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+#app hello
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    // Hello,
+  },
+};
+</script>
+
+<style lang='scss'>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
