de64335384d302f2641dacede430caed4c71a34e
xandeer <appdwj@icloud.com>
2017-02-12 15:30:22 +0800

    feat(location): finish simon game

--- simon/src/components/simon/simon.vue
+++ simon/src/components/simon/simon.vue
@@ -1,33 +1,35 @@
 <template lang='pug'>
-  .simon
-    .simon-item(@mousedown='light0 = true', @mouseup='light0 = false', :class='light0 ? "light" : ""')
-      .circle.tl.bg_red(@click='play(0)', :class='isPlayerTurn ? "enabled" : ""')
-    .simon-item(:class='light1 ? "light" : ""')
-      .circle.tr.bg_yellow(@click='play(1)', :class='isPlayerTurn ? "enabled" : ""')
-    .simon-item(:class='light2 ? "light" : ""')
-      .circle.bl.bg_blue(@click='play(2)', :class='isPlayerTurn ? "enabled" : ""')
-    .simon-item(:class='light3 ? "light" : ""')
-      .circle.br.bg_green(@click='play(3)', :class='isPlayerTurn ? "enabled" : ""')
-    .simon-item.simon-ctl
-      .ctl-top Simon
-      .ctl-middle
-        .middle-item
-          .count-val {{ status }}
-          .ctl-label COUNT
-        .middle-item
-          .ctl-btn.bg_red(:class='sw === "on" ? "enabled" : ""', @click='startGame')
-          .ctl-label START
-        .middle-item
-          .ctl-btn.bg_green(@click='toggleStrict', :class='strictState')
-          .ctl-label STRICT
-      .ctl-bottom
-        .ctl-label OFF
-        .switch(@click='toggleSwitch')
-          .switch-inner(:class='sw')
-        .ctl-label ON
+.simon
+  .simon-item(:class='light0 ? "light" : ""')
+    .circle.tl.bg_red(@click='play(0)', :class='playerTurn ? "enabled" : ""')
+  .simon-item(:class='light1 ? "light" : ""')
+    .circle.tr.bg_yellow(@click='play(1)', :class='playerTurn ? "enabled" : ""')
+  .simon-item(:class='light2 ? "light" : ""')
+    .circle.bl.bg_blue(@click='play(2)', :class='playerTurn ? "enabled" : ""')
+  .simon-item(:class='light3 ? "light" : ""')
+    .circle.br.bg_green(@click='play(3)', :class='playerTurn ? "enabled" : ""')
+  .simon-item.simon-ctl
+    .ctl-top Simon
+    .ctl-middle
+      .middle-item
+        .count-val {{ status }}
+        .ctl-label COUNT
+      .middle-item
+        .ctl-btn.bg_red(:class='sw === "on" ? "enabled" : ""', @click='startGame')
+        .ctl-label START
+      .middle-item
+        .ctl-btn.bg_green(@click='toggleStrict', :class='strictState')
+        .ctl-label STRICT
+    .ctl-bottom
+      .ctl-label OFF
+      .switch(@click='toggleSwitch')
+        .switch-inner(:class='sw')
+      .ctl-label ON
 </template>
 
 <script>
+import { Howl } from 'howler';
+
 function getRandom() {
   return Math.floor(Math.random() * 4);
 }
@@ -43,11 +45,16 @@ export default {
       light1: false,
       light2: false,
       light3: false,
-      timer: null,
+      intervalId: null,
+      interval: 1000,
+      timeoutId: null,
+      timeout: 3000,
       order: [],
       compareOrder: [],
-      isPlayerTurn: false,
-      isError: false,
+      playerTurn: false,
+      error: false,
+      win: false,
+      audios: [],
     };
   },
   computed: {
@@ -57,23 +64,51 @@ export default {
       return state;
     },
     status() {
-      if (this.isError) {
+      if (this.win) {
+        return 'win';
+      } else if (this.error) {
         return '! !';
       } else if (!this.start) {
         return '- -';
       }
       return this.count;
     },
+    whichLight() {
+      let ret = -1;
+      for (let i = 0; i < 4; i += 1) {
+        if (this[`light${i}`]) {
+          ret = i;
+          break;
+        }
+      }
+      return ret;
+    },
+  },
+  watch: {
+    whichLight(val) {
+      this.audios.forEach((audio) => {
+        audio.stop();
+      });
+      if (val !== -1) {
+        this.audios[val].play();
+      }
+    },
   },
   methods: {
     init() {
-      this.isPlayerTurn = false;
-      this.isError = false;
+      this.playerTurn = false;
+      this.error = false;
+      this.win = false;
       this.count = 1;
       this.order = [];
       this.compareOrder = [];
-      if (this.timer) {
-        clearInterval(this.timer);
+      this.interval = 1000;
+      this.timeout = 3000;
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+      }
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
       }
       for (let i = 0; i < 4; i += 1) {
         this[`light${i}`] = false;
@@ -88,71 +123,111 @@ export default {
     toggleStrict() {
       this.strict = this.sw === 'on' ? !this.strict : this.strict;
     },
-    whichLight() {
-      let ret = -1;
-      for (let i = 0; i < 4; i += 1) {
-        if (this[`light${i}`]) {
-          ret = i;
-          break;
+    gameLoop() {
+      let index = 0;
+      let errorTimes = 2;
+
+      this.intervalId = setInterval(() => {
+        if (this.whichLight !== -1) {
+          this[`light${this.whichLight}`] = false;
+        } else if (!this.playerTurn && !this.error) {
+          this[`light${this.order[index]}`] = true;
+          index += 1;
         }
-      }
-      return ret;
+
+        if (index === 10) {
+          this.interval = 500;
+        }
+
+        if (this.error && errorTimes > 0) {
+          errorTimes -= 1;
+        } else if (this.error) {
+          errorTimes = 1;
+          this.error = false;
+        }
+
+        if (this.win) {
+          clearInterval(this.intervalId);
+        }
+
+        // player turn
+        if (index >= this.count) {
+          this.compareOrder = this.order.slice(0);
+          this.playerTurn = true;
+          index = 0;
+          this.timeoutId = setTimeout(() => {
+            this.fail();
+          }, this.timeout);
+        }
+      }, this.interval);
     },
     startGame() {
       if (this.sw === 'off') {
         return;
       }
+
+      // init game
       this.init();
-
-      let index = 0;
-      let errorTimes = 2;
       this.order.push(getRandom());
-
       this.start = true;
       this.count = 1;
-      this.timer = setInterval(() => {
-        const lightIndex = this.whichLight();
-        if (lightIndex !== -1) {
-          this[`light${lightIndex}`] = false;
-        } else if (!this.isPlayerTurn && !this.isError) {
-          this[`light${this.order[index]}`] = true;
-          index += 1;
-        }
 
-        if (this.isError && errorTimes > 0) {
-          errorTimes -= 1;
-        } else if (this.isError) {
-          errorTimes = 1;
-          this.isError = false;
-        }
-
-        if (index >= this.count) {
-          this.compareOrder = this.order.slice(0);
-          this.isPlayerTurn = true;
-          index = 0;
-        }
-      }, 500);
+      this.gameLoop();
     },
-    play(num) {
-      if (this.isPlayerTurn) {
-        if (num !== this.compareOrder.shift()) {
-          this.isError = true;
-          this.isPlayerTurn = false;
-          this.compareOrder = [];
-          if (this.strict) {
-            this.count = 1;
-            this.order = [];
-            this.order.push(getRandom());
-          }
-        }
-
-        if (!this.isError && this.compareOrder.length === 0) {
-          this.order.push(getRandom());
-          this.count += 1;
-          this.isPlayerTurn = false;
-        }
+    fail() {
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+      }
+      this.error = true;
+      this.playerTurn = false;
+      this.compareOrder = [];
+      if (this.strict) {
+        this.count = 1;
+        this.order = [];
+        this.order.push(getRandom());
       }
     },
+    play(num) {
+      if (!this.playerTurn) {
+        return;
+      }
+
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+        this.timeoutId = setTimeout(() => {
+          this.fail();
+        }, this.timeout);
+      }
+      this[`light${num}`] = true;
+
+      if (num !== this.compareOrder.shift()) {
+        this.fail();
+      }
+
+      if (!this.error && this.compareOrder.length === 0) {
+        if (this.order.length === 20) {
+          this.win = true;
+          return;
+        }
+        this.order.push(getRandom());
+        this.count += 1;
+        this.playerTurn = false;
+        clearTimeout(this.timeoutId);
+      }
+    },
+  },
+  created() {
+    for (let i = 1; i < 5; i += 1) {
+      const url = `https://s3.amazonaws.com/freecodecamp/simonSound${i}.mp3`;
+      this.audios.push(new Howl({
+        src: url,
+        html5: true,
+      }));
+    }
+  },
+  beforeDestroy() {
+    this.timeoutId = this.timeoutId && clearTimeout(this.timeoutId);
+    this.intervalId = this.intervalId && clearInterval(this.intervalId);
   },
 };
 </script>
